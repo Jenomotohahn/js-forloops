d947f4c9247b72cc03fa72903dcb558eaba39388
Jennifer Enomoto-Hahn <hugomcmittens@Jennifers-MacBook-Air.local>
2018-07-09 21:54:39 -1000

    Finished #3A - worked well

--- exercises.js
+++ exercises.js
@@ -64,7 +64,14 @@ for (var i = 0; i < presidents.length; i++){
   console.log('The value at '+ i + ' is '+ presidents[i]+ '.');
 }
 
+console.log(presidents.length);
 
+
+function leaders(person){
+  for( var i = 0; i < person.length; i++){
+  console.log('President ' + person[i] + ' was a great leader.' );
+}
+}
 
 
 
